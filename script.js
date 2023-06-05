5573d8908d7d82713dc30f18cb90a277abfdcdcd
devCadet <satyam.karn3629@gmail.com>
2023-06-05 22:34:46 +0530

    first version of simple ToDo

---
+++ script.js
@@ -0,0 +1,9 @@
+function addTask() {
+    const task = document.getElementById('input').value; 
+    document.getElementById('task').innerHTML =`Task 1: ${task} ` ;
+    document.getElementById('').style.display = 'none'
+    
+    
+}
+
+
